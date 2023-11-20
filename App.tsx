15fbe79c79ee383d48f230b486bf20a66b77c7da
DaEnV <120397126+daenv@users.noreply.github.com>
2023-11-20 17:49:40 +0700

    update tab navigator

--- App.tsx
+++ App.tsx
@@ -1,24 +1,20 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import SplashScreen from "react-native-splash-screen";
-import TabNavigator from "./src/navigators/TabNavigator";
-import DetailsScreen from "./src/screens/DetailsScreen";
-import PaymentScreen from "./src/screens/PaymentScreen";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SplashScreen from 'react-native-splash-screen';
+import TabNavigator from './src/navigators/TabNavigator';
+import DetailsScreen from './src/screens/DetailsScreen';
+import PaymentScreen from './src/screens/PaymentScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="Tab"
-          component={TabNavigator}
-          options={{ animation: "slide_from_bottom" }}
-        />
-        <Stack.Screen name="Details" component={DetailsScreen} options={{ animation: "slide_from_bottom" }} />
-        <Stack.Screen name="Payment" component={PaymentScreen} options={{ animation: "slide_from_bottom" }} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Tab" component={TabNavigator} options={{ animation: 'slide_from_bottom' }} />
+                <Stack.Screen name="Details" component={DetailsScreen} options={{ animation: 'slide_from_bottom' }} />
+                <Stack.Screen name="Payment" component={PaymentScreen} options={{ animation: 'slide_from_bottom' }} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
--- src/navigators/TabNavigator.tsx
+++ src/navigators/TabNavigator.tsx
@@ -1,14 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { COLORS } from '../theme/theme';
+import HomeScreen from '../screens/HomeScreen';
+import { BlurView } from '@react-native-community/blur';
+
+const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
-  return (
-    <View>
-      <Text>TabNavigator</Text>
-    </View>
-  )
-}
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                tabBarHideOnKeyboard: true,
+                headerShown: false,
+                tabBarShowLabel: false,
+                tabBarStyle: styles.tabBarStyle,
+            }}
+        >
+            <Tab.Screen name="Home" component={HomeScreen}></Tab.Screen>
+            
+        </Tab.Navigator>
+    );
+};
 
-export default TabNavigator
+export default TabNavigator;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    tabBarStyle: {
+        height: 80,
+        position: 'absolute',
+        backgroundColor: COLORS.primaryBlackRGBA,
+        borderTopWidth: 0,
+        elevation: 0,
+        borderTopColor: 'transparent',
+    },
+    BlurViewStyles: {
+        position: 'absolute',
+        top: 0,
+        bottom: 0,
+        left: 0,
+        right: 0,
+    },
+});
